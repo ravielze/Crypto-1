d39efcbaad83b832c5f105f6ca761d4fe703d132
Christopher Justine William <13519006@std.stei.itb.ac.id>
2023-01-29 11:15:19 +0700

    feat: add playfair cipher

---
+++ internal/playfair/playfair.go
@@ -0,0 +1,86 @@
+package playfair
+
+import (
+	"github.com/ravielze/Crypto-1/internal"
+	"github.com/ravielze/Crypto-1/internal/utils"
+	"strings"
+)
+
+type (
+	playfair struct {
+		key string
+	}
+)
+
+func NewPlayfair(key string) internal.AlphabetCipher {
+	return &playfair{key}
+}
+
+func (p *playfair) Metadata() map[string]any {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p *playfair) Key() string {
+	return p.key
+}
+
+func (p *playfair) Encrypt(plaintext string) string {
+	key := utils.Normalize(p.key)
+	plaintext = utils.Normalize(plaintext)
+	table := utils.GenerateTable(key)
+	offset := 1
+
+	var result []rune
+	for i := 0; i < len(plaintext); {
+		var char1, char2 rune
+		if i == len(plaintext)-1 {
+			char1, char2 = rune(plaintext[i]), 'X'
+		} else {
+			char1, char2 = rune(plaintext[i]), rune(plaintext[i+1])
+		}
+
+		if char1 == char2 {
+			char2 = 'X'
+			i++
+		} else {
+			i += 2
+		}
+
+		i1, i2 := table.GetIndex(char1, char2)
+		if table.IsSameRow(i1, i2) {
+			i1, i2 = table.ShiftHorizontal(i1, i2, offset)
+		} else if table.IsSameColumn(i1, i2) {
+			i1, i2 = table.ShiftVertical(i1, i2, offset)
+		} else {
+			i1, i2 = table.ShiftRectangle(i1, i2)
+		}
+
+		result = append(result, table[i1], table[i2])
+	}
+	return string(result)
+}
+
+func (p *playfair) Decrypt(ciphertext string) string {
+	key := utils.Normalize(p.key)
+	ciphertext = utils.Normalize(ciphertext)
+	table := utils.GenerateTable(key)
+	offset := 1
+
+	var result []rune
+	for i := 0; i < len(ciphertext); i += 2 {
+		char1, char2 := rune(ciphertext[i]), rune(ciphertext[i+1])
+
+		i1, i2 := table.GetIndex(char1, char2)
+		if table.IsSameRow(i1, i2) {
+			i1, i2 = table.ShiftHorizontal(i1, i2, -1*offset)
+		} else if table.IsSameColumn(i1, i2) {
+			i1, i2 = table.ShiftVertical(i1, i2, -1*offset)
+		} else {
+			i1, i2 = table.ShiftRectangle(i1, i2)
+		}
+
+		result = append(result, table[i1], table[i2])
+	}
+	return strings.ReplaceAll(string(result), "X", "")
+}
---
+++ internal/utils/table.go
@@ -0,0 +1,87 @@
+package utils
+
+type Table []rune
+
+func GenerateTable(key string) Table {
+	var table Table
+	used := make(map[rune]bool)
+
+	for _, char := range key {
+		table.Fill(char, used)
+	}
+
+	for char := 'A'; char <= 'Z'; char++ {
+		table.Fill(char, used)
+	}
+
+	return table
+}
+
+func (table *Table) Fill(char rune, used map[rune]bool) {
+	if used[char] {
+		return
+	}
+	if char == 'J' {
+		return
+	}
+	used[char] = true
+	*table = append(*table, char)
+}
+
+func (table *Table) GetIndex(char1, char2 rune) (int, int) {
+	i1, i2 := -1, -1
+	for index, c := range *table {
+		if c == char1 {
+			i1 = index
+		} else if c == char2 {
+			i2 = index
+		}
+	}
+	return i1, i2
+}
+
+func (table *Table) IsSameRow(i1, i2 int) bool {
+	return i1/5 == i2/5
+}
+
+func (table *Table) IsSameColumn(i1, i2 int) bool {
+	return i1%5 == i2%5
+}
+
+func (table *Table) ShiftHorizontal(i1, i2 int, n int) (int, int) {
+	row1, col1 := table.convertIndexToRowAndColumn(i1)
+	row2, col2 := table.convertIndexToRowAndColumn(i2)
+
+	i1 = table.convertRowAndColumnToIndex(row1, (col1+n)%5)
+	i2 = table.convertRowAndColumnToIndex(row2, (col2+n)%5)
+
+	return i1, i2
+}
+
+func (table *Table) ShiftVertical(i1, i2 int, n int) (int, int) {
+	row1, col1 := table.convertIndexToRowAndColumn(i1)
+	row2, col2 := table.convertIndexToRowAndColumn(i2)
+
+	i1 = table.convertRowAndColumnToIndex((row1+n)%5, col1)
+	i2 = table.convertRowAndColumnToIndex((row2+n)%5, col2)
+
+	return i1, i2
+}
+
+func (table *Table) ShiftRectangle(i1, i2 int) (int, int) {
+	row1, col1 := table.convertIndexToRowAndColumn(i1)
+	row2, col2 := table.convertIndexToRowAndColumn(i2)
+
+	i1 = table.convertRowAndColumnToIndex(row1, col2)
+	i2 = table.convertRowAndColumnToIndex(row2, col1)
+
+	return i1, i2
+}
+
+func (table *Table) convertIndexToRowAndColumn(index int) (int, int) {
+	return index / 5, index % 5
+}
+
+func (table *Table) convertRowAndColumnToIndex(row, column int) int {
+	return row*5 + column
+}
